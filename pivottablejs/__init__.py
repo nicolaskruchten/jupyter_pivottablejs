e86f43e674f2a7d928d0266130159d8dc36f6b25
Nicolas Kruchten <nicolas@datacratic.com>
2017-05-06 22:43:05 -0400

    upgrade to PivotTable.js 2.13.0

--- pivottablejs/__init__.py
+++ pivottablejs/__init__.py
@@ -1,27 +1,23 @@
-# %install_ext http://nicolas.kruchten.com/pivottable/jupyter/pivottablejs.py
-# %load_ext pivottablejs
-# %pivottablejs data_frame
-
-
 template = """
 <!DOCTYPE html>
 <html>
     <head>
         <title>PivotTable.js</title>
-        
+
         <!-- external libs from cdnjs -->
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
 
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/pivot.min.css">
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/pivot.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/d3_renderers.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/c3_renderers.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/export_renderers.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.13.0/pivot.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.13.0/pivot.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.13.0/d3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.13.0/c3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.13.0/export_renderers.min.js"></script>
 
         <style>
             body {font-family: Verdana;}
@@ -39,6 +35,7 @@ template = """
             .tick line {stroke: white;}
             .c3-axis path {stroke: grey;}
             .c3-circle { opacity: 1 !important; }
+            .c3-xgrid-focus {visibility: hidden !important;}
         </style>
     </head>
     <body>
@@ -47,13 +44,13 @@ template = """
                 if(window.location != window.parent.location)
                     $("<a>", {target:"_blank", href:""})
                         .text("[pop out]").prependTo($("body"));
-                    
-                $("#output").pivotUI( 
-                    $.csv.toArrays($("#output").text()), 
-                    { 
+
+                $("#output").pivotUI(
+                    $.csv.toArrays($("#output").text()),
+                    {
                         renderers: $.extend(
-                            $.pivotUtilities.renderers, 
-                            $.pivotUtilities.c3_renderers, 
+                            $.pivotUtilities.renderers,
+                            $.pivotUtilities.c3_renderers,
                             $.pivotUtilities.d3_renderers,
                             $.pivotUtilities.export_renderers
                             ),
@@ -73,4 +70,4 @@ def pivot_ui(df, outfile_path = "pivottablejs.html", width="100%", height="500")
     with open(outfile_path, 'w') as outfile:
         outfile.write(template % df.to_csv())
     return IFrame(src=outfile_path, width=width, height=height)
-        
+
